884e294bac848f5a1bf34abdeffaeaf67da0bbc3
Afif Sohaili <afifnajib@gmail.com>
2020-08-24 20:20:08 +0800

    Add validation using Yup, add back button for resubmitting.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,21 @@ import styled from 'styled-components';
 import {SPACING_L} from "./styles";
 import {signup} from "./api/signup";
 import {useFormik} from "formik";
+import * as Yup from "yup";
 
 const Form = styled.form`
     padding: ${SPACING_L};
 `
 
 const TEST_CAMPAIGN_UUID = '46aa3270-d2ee-11ea-a9f0-e9a68ccff42a'
+
+const SignupSchema = Yup.object().shape({
+    firstName: Yup.string().required('Required'),
+    lastName: Yup.string().required('Required'),
+    email: Yup.string().email('Invalid email address').required('Required'),
+    password: Yup.string().min(8, 'Password must be at least 8 characters long.').required('Required'),
+    passwordRepeat: Yup.string().min(8, 'Password must be at least 8 characters long.').required('Required')
+})
 
 function App() {
     const [hasSignedUp, setHasSignedUp] = useState(false)
@@ -23,10 +32,12 @@ function App() {
             password: '',
             passwordRepeat: ''
         },
+        validationSchema: SignupSchema,
         onSubmit: async values => {
             const {firstName, lastName, email, password, passwordRepeat} = values
             if (password !== passwordRepeat) {
                 formik.errors.password = 'The two password fields do not match.'
+                formik.errors.passwordRepeat = 'The two password fields do not match.'
                 return
             }
             await signup({campaignUuid: TEST_CAMPAIGN_UUID, firstName, lastName, email, password})
@@ -36,25 +47,28 @@ function App() {
     })
 
     if (hasSignedUp) {
-        return <p>You've successfully signed up.</p>
+        return <>
+            <p>You've successfully signed up.</p>
+            <a href='#' onClick={() => setHasSignedUp(false)}>&laquo; Back</a>
+        </>
     }
 
     return (
         <>
-            <Form onSubmit={formik.handleSubmit}>
-                <InputField labelText='First Name' id='first-name' error={formik.errors.firstName}>
+            <Form onSubmit={e => formik.handleSubmit(e)}>
+                <InputField labelText='First Name' id='first-name' error={formik.touched.firstName && formik.errors.firstName}>
                     <input name='firstName' type='text' id='first-name' onChange={formik.handleChange}/>
                 </InputField>
-                <InputField labelText={'Last Name'} id='last-name' error={formik.errors.lastName}>
+                <InputField labelText={'Last Name'} id='last-name' error={formik.touched.lastName && formik.errors.lastName}>
                     <input name='lastName' type='text' id='last-name' onChange={formik.handleChange}/>
                 </InputField>
-                <InputField labelText={'Email address'} id='email' error={formik.errors.email}>
+                <InputField labelText={'Email address'} id='email' error={formik.touched.email && formik.errors.email}>
                     <input name='email' type='text' id='email' onChange={formik.handleChange}/>
                 </InputField>
-                <InputField labelText={'Password'} id='password' error={formik.errors.password}>
+                <InputField labelText={'Password'} id='password' error={formik.touched.password && formik.errors.password}>
                     <input name='password' type='password' id='password' onChange={formik.handleChange}/>
                 </InputField>
-                <InputField labelText={'Password'} id='password' error={formik.errors.password}>
+                <InputField labelText={'Password'} id='password' error={formik.touched.passwordRepeat && formik.errors.passwordRepeat}>
                     <input name='passwordRepeat' type='password' id='password-repeat' onChange={formik.handleChange}/>
                 </InputField>
 
